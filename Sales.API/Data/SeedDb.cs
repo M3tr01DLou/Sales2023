afc22ca447366ca8b00fc168cee56001eb5c93a9
PANEL\luisjavier.diez <luisjavier.diez@panel.es>
2023-09-26 17:20:10 +0200

    Add SeedDb

---
+++ Sales.API/Data/SeedDb.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _dataContext;
+
+        public SeedDb(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task SeedAsync()
+        { 
+            await _dataContext.Database.EnsureCreatedAsync();
+            await CheckCountriesAsync();
+            await CheckCategoriesAsync();
+        }
+
+        private async Task CheckCategoriesAsync()
+        {
+            if (!_dataContext.Categories.Any())
+            {
+                _dataContext.Categories.Add(new Category { Name = "Apple" });
+                _dataContext.Categories.Add(new Category { Name = "Calzado" });
+                _dataContext.Categories.Add(new Category { Name = "informática" });
+                _dataContext.Categories.Add(new Category { Name = "Hogar" });
+                _dataContext.Categories.Add(new Category { Name = "Automovil" });
+            }
+
+            await _dataContext.SaveChangesAsync();
+        }
+
+        private async Task CheckCountriesAsync()
+        {
+            if (!_dataContext.Countries.Any())
+            {
+                _dataContext.Countries.Add(new Country { Name = "España" });
+                _dataContext.Countries.Add(new Country { Name = "Portugal" });
+                _dataContext.Countries.Add(new Country { Name = "Francia" });
+                _dataContext.Countries.Add(new Country { Name = "Alemania" });
+            }
+
+            await _dataContext.SaveChangesAsync();
+        }
+
+    }
+}
