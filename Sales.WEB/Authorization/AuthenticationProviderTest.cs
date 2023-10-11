8ee28c4a14cb8b509b220534d47b1e5d136ed1b9
PANEL\luisjavier.diez <luisjavier.diez@panel.es>
2023-10-11 16:50:55 +0200

    Add securization web

---
+++ Sales.WEB/Authorization/AuthenticationProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Authorization
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var louUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Lou"),
+                new Claim("LastName", "González"),
+                new Claim(ClaimTypes.Name, "lou@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+            authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+
+        }
+    }
+}
--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -1,7 +1,9 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales.WEB;
+using Sales.WEB.Authorization;
 using Sales.WEB.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,8 +11,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7204/") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7204/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 
 await builder.Build().RunAsync();
